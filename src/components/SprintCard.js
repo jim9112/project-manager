6c308b3f6f0a49214d636d07ba00405e40260b22
jim9112 <jim9112@gmail.com>
2020-11-30 16:12:28 -0500

    cross out to do items when done

--- src/components/SprintCard.js
+++ src/components/SprintCard.js
@@ -1,4 +1,15 @@
-import { Paper, Typography, makeStyles, Divider } from '@material-ui/core';
+import {
+  Paper,
+  Typography,
+  makeStyles,
+  Divider,
+  List,
+  ListItemIcon,
+  ListItem,
+  ListItemText,
+  Checkbox,
+} from '@material-ui/core';
+import { useState } from 'react';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -8,17 +19,65 @@ const useStyles = makeStyles((theme) => ({
   title: {
     textAlign: 'center',
   },
+  done: {
+    textDecoration: 'line-through',
+  },
 }));
 
 const SprintCard = () => {
   const classes = useStyles();
-
+  const [todoList, setTodoList] = useState([
+    {
+      checked: false,
+      task: 'First Task',
+    },
+    {
+      checked: true,
+      task: 'Second Task',
+    },
+    {
+      checked: false,
+      task: 'Third Task',
+    },
+  ]);
+  // line through to do items when checked
+  const onCheck = (task, i) => {
+    const updateList = todoList;
+    if (task.checked === true) {
+      updateList[i].checked = false;
+    } else {
+      updateList[i].checked = true;
+    }
+    setTodoList([...updateList]);
+  };
+  // add form dialog for new task
   return (
     <Paper className={classes.paper}>
       <Typography className={classes.title} variant="h4">
         Current Sprint
       </Typography>
       <Divider />
+      <Typography varient="h3" align="center">
+        Build Today Page
+      </Typography>
+      <List>
+        {todoList.map((task, i) => {
+          return (
+            <ListItem key={i} divider={true}>
+              <ListItemIcon>
+                <Checkbox
+                  checked={task.checked}
+                  onChange={() => onCheck(task, i)}
+                  color="primary"
+                />
+              </ListItemIcon>
+              <ListItemText className={task.checked ? classes.done : ''}>
+                {task.task}
+              </ListItemText>
+            </ListItem>
+          );
+        })}
+      </List>
     </Paper>
   );
 };
--- src/pages/TodayPage.js
+++ src/pages/TodayPage.js
@@ -1,5 +1,4 @@
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import BugsCard from '../components/BugsCard';
 import NotesCard from '../components/NotesCard';
