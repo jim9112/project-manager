2c560cfbe3b59833af0ee085ba021e1582e0816e
jim9112 <jim9112@gmail.com>
2020-12-03 12:08:13 -0500

    Add add project icon to home

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,9 +1,28 @@
-import { Card, CardHeader } from '@material-ui/core';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Divider,
+  Typography,
+} from '@material-ui/core';
 
 const ProjectCard = () => {
   return (
     <Card>
       <CardHeader title="Project One" />
+      <Divider />
+      <CardContent>
+        <Typography variant="body1">
+          Here is some information about my Project.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button color="primary" variant="contained">
+          Open
+        </Button>
+      </CardActions>
     </Card>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,33 @@
+import { Fab, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
 import AppBar from '../components/AppBar';
-
+import ProjectCard from '../components/ProjectCard';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginTop: '1rem',
+  },
+}));
 const Home = ({ history }) => {
+  const classes = useStyles();
   return (
     <>
       <AppBar history={history} />
-      <h1>Home</h1>
+      <Grid className={classes.root} container spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={2}>
+            <Grid item xs={12} sm={12} md={4}>
+              <ProjectCard />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4}>
+              <Fab color="primary" aria-label="add">
+                <AddIcon />
+              </Fab>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 };
