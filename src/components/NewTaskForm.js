9b4aabc9553f6de787fe90b6a5d1f47a6394d64d
jim9112 <jim9112@gmail.com>
2020-12-14 13:47:29 -0500

    new task form now writes to database

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,61 +1,58 @@
 import {
-    makeStyles,
-    TextField,
-    DialogActions,
-    Button,
-  } from '@material-ui/core';
-  import { useContext, useEffect } from 'react';
-  import useForm from '../utils/useForm';
-  import UserContext from '../context/UserContext';
-  
-  const useStyles = makeStyles((theme) => ({
-    newTextForm: {
-      padding: '1rem',
-    },
-  }));
-  
-  const NewTaskForm = ({ setOpen }) => {
-    const classes = useStyles();
-  
-    const [handleInput, input, setInput] = useForm();
-    const {todoList, setTodoList} = useContext(UserContext);
-  
-    // set standard data for not that doesnt come from form input
-    useEffect(() => {
-      setInput({...input, checked: false, date: Date.now()})
-    },[])
-    
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const updateList = todoList;
-      updateList.unshift({ ...input });
-      setTodoList([...updateList]);
-      setOpen(false);
-    };
-  
-    return (
-      <form className={classes.newTextForm} onSubmit={handleSubmit}>
-        <TextField
-          autoFocus
-          required={true}
-          margin="dense"
-          id="task"
-          label="New Task"
-          type="text"
-          fullWidth
-          onChange={handleInput}
-        />
-        <DialogActions>
-          <Button onClick={() => setOpen(false)} color="primary">
-            Cancel
-          </Button>
-          <Button type="submit" color="primary">
-            Add Task
-          </Button>
-        </DialogActions>
-      </form>
-    );
+  makeStyles,
+  TextField,
+  DialogActions,
+  Button,
+} from '@material-ui/core';
+import { useEffect } from 'react';
+import useForm from '../utils/useForm';
+
+import useAddToProjectSubCollection from '../utils/useAddToProjectSubCollection';
+
+const useStyles = makeStyles((theme) => ({
+  newTextForm: {
+    padding: '1rem',
+  },
+}));
+
+const NewTaskForm = ({ setOpen }) => {
+  const classes = useStyles();
+  const [handleInput, input, setInput] = useForm();
+  const addToCollection = useAddToProjectSubCollection('Tasks');
+
+  // set standard data for not that doesnt come from form input
+  useEffect(() => {
+    setInput({ ...input, checked: false, date: Date.now() });
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addToCollection(input);
+    setOpen(false);
   };
-  
-  export default NewTaskForm;
-  
+
+  return (
+    <form className={classes.newTextForm} onSubmit={handleSubmit}>
+      <TextField
+        autoFocus
+        required={true}
+        margin="dense"
+        id="task"
+        label="New Task"
+        type="text"
+        fullWidth
+        onChange={handleInput}
+      />
+      <DialogActions>
+        <Button onClick={() => setOpen(false)} color="primary">
+          Cancel
+        </Button>
+        <Button type="submit" color="primary">
+          Add Task
+        </Button>
+      </DialogActions>
+    </form>
+  );
+};
+
+export default NewTaskForm;
