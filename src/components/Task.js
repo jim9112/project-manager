9f17cbed77e7fbb1c9cbf738901056b3dce46a3a
jim9112 <jim9112@gmail.com>
2020-12-15 16:06:36 -0500

    Tasks cross off when checked

--- src/components/Task.js
+++ src/components/Task.js
@@ -5,6 +5,10 @@ import {
   ListItemText,
   Checkbox,
 } from '@material-ui/core';
+import { firestore } from '../firebaseIndex';
+import AuthContext from '../context/AuthContext';
+import UserContext from '../context/UserContext';
+import { useContext } from 'react';
 
 const useStyles = makeStyles((theme) => ({
   done: {
@@ -14,11 +18,38 @@ const useStyles = makeStyles((theme) => ({
 
 const Task = ({ task }) => {
   const classes = useStyles();
+  const { user } = useContext(AuthContext);
+  const { currentProject } = useContext(UserContext);
 
   // line through to do items when checked
   const onCheck = (task) => {
-    console.log('Checked');
+    if (!task.checked) {
+      firestore
+        .collection('users')
+        .doc(user.uid)
+        .collection('Projects')
+        .doc(currentProject)
+        .collection('Tasks')
+        .doc(task.id)
+        .update({
+          checked: true,
+        })
+        .catch((err) => console.log(err));
+    } else if (task.checked) {
+      firestore
+        .collection('users')
+        .doc(user.uid)
+        .collection('Projects')
+        .doc(currentProject)
+        .collection('Tasks')
+        .doc(task.id)
+        .update({
+          checked: false,
+        })
+        .catch((err) => console.log(err));
+    }
   };
+
   return (
     <ListItem divider={true}>
       <ListItemIcon>
