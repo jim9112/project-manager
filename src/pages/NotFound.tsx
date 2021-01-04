c21a812f11f0ff0fa8ebbba3b42618ff98073498
jim9112 <jim9112@gmail.com>
2021-01-04 09:18:28 -0500

    add 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,38 @@
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper, Grid, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    height: '500px',
+  },
+  paper: {
+    textAlign: 'center',
+    padding: '1rem',
+  },
+  container: {
+    height: '100%',
+  },
+}));
+
 const NotFound: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <p>404 Page Not found</p>
+    <div className={classes.root}>
+      <Grid
+        className={classes.container}
+        container
+        alignContent="center"
+        justify="center">
+        <Grid item xs={12} sm={12} md={6}>
+          <Paper className={classes.paper}>
+            <Typography variant="h3">404 Page Not found</Typography>
+            <Link to="/"> Home </Link>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 };
